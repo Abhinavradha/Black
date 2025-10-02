89986bd25aa6bf2d2b27571f5bfc5f14102ae6fa
Abhinavradha <130545673+Abhinavradha@users.noreply.github.com>
2025-10-02 10:44:24 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,8 @@
-api_id = 35814071 #change_with_your_Api_id.
-api_hash = "80c231f0e8704995b8259d06965n4351" #change_with_your_Api_hash.
-bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
-auth_users = [999741495] #change_with_your_user_id.
-sudo_user = 1713924419 #change_with_your_user_id.
-log_channel = -1002154281532 #change_with_your_channel_id.
-txt_channel = -1002154281532 #change_with_your_channel_id.
+api_id = 28765417 #change_with_your_Api_id.
+api_hash = "0e322ff260eddcb2860956069902929f" #change_with_your_Api_hash.
+bot_token = "8490053601:AAHSb3x6Ibmf44UrpZm_5inEmuPVWJ-NeMU" #change_with_your_bot_token.
+auth_users = [8490053601] #change_with_your_user_id.
+sudo_user = 6066532924 #change_with_your_user_id.
+log_channel = -1003082169405 #change_with_your_channel_id.
+txt_channel = -1003082169405 #change_with_your_channel_id.
+
